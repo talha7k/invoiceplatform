3fbed84fa26f0bb4b0909ccd3f10b86f0a071336
Talha7k <talha7k@gmail.com>
2024-11-21 21:05:36 +0100

    added turnery

--- app/actions.ts
+++ app/actions.ts
@@ -86,7 +86,10 @@ export async function createInvoice(prevState: any, formData: FormData) {
         amount: submission.value.total,
         currency: submission.value.currency as any,
       }),
-      invoiceLink: `http://localhost:3000/api/invoice/${data.id}`,
+      invoiceLink:
+        process.env.NODE_ENV !== "production"
+          ? `http://localhost:3000/api/invoice/${data.id}`
+          : `https://invoice-marshal.vercel.app/api/invoice/${data.id}`,
     },
   });
 
@@ -149,7 +152,10 @@ export async function editInvoice(prevState: any, formData: FormData) {
         amount: submission.value.total,
         currency: submission.value.currency as any,
       }),
-      invoiceLink: `http://localhost:3000/api/invoice/${data.id}`,
+      invoiceLink:
+        process.env.NODE_ENV !== "production"
+          ? `http://localhost:3000/api/invoice/${data.id}`
+          : `https://invoice-marshal.vercel.app/api/invoice/${data.id}`,
     },
   });
 
